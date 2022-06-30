848322cee1a914f71687151b605be815231dfc24
juanfernando.toro <juanfernando.toro@polimi.it>
2022-06-30 16:51:03 +0200

    Outlier rejection methods

--- notebook/src/python/wqpFunctions.py
+++ notebook/src/python/wqpFunctions.py
@@ -3,7 +3,11 @@ import csv
 from datetime import datetime
 import numpy as np
 import pandas as pd
+import fiona
 import rasterio
+from rasterio.merge import merge
+import rasterio.mask
+from rasterio.io import MemoryFile
 from rasterstats import zonal_stats, point_query
 
 class wqp:
@@ -44,12 +48,12 @@ class wqp:
             # And then change the band count to 1, set the
             # dtype to uint8, and specify LZW compression.
             profile.update(
-                dtype=rasterio.uint8,
+                dtype=rasterio.float64,
                 count=1,
                 compress='lzw')
 
             with rasterio.open(os.path.join(out_path,self.name+'.tif'), 'w', **profile) as dst:
-                dst.write(array.astype(rasterio.uint8), 1)
+                dst.write(array.astype(rasterio.float64), 1)
     
     # Compute the zonal statistics for a reference raster and polygon
     def computeStatistics(self, vectorData, nameField, stats, nodata):
@@ -76,8 +80,12 @@ class wqp:
             x = point.xy[0][0]
             y = point.xy[1][0]
             row, col = self.image.index(x,y)
+            try:
+                value = self.image.read(1)[row,col]
+            except:
+                print(x,',',y,' coordinates are out of the image')
             
-            o = [x,y,row,col,self.image.read(1)[row,col]]
+            o = [x,y,row,col,value]
             
             obs.append(o)
         
@@ -113,8 +121,131 @@ class wqp:
         df = df.reindex(columns = con)
         
         return df
-        
-        
+    
+    #OUTLIER REJECTION FUNCTIONS
+    """
+    DEFINE THE PROCESSING AND STORING FUNCTIONS
+    """
+    def saveMaskedImage(out_path,in_file,data,transf):
+        with rasterio.open(in_file) as src:
+            profile = src.profile.copy()
+            profile.update({
+                    'dtype': 'float64',
+                    'height': data.shape[0],
+                    'width': data.shape[1],
+                    'transform': transf
+             })  
 
+        with rasterio.open(out_path, 'w', **profile) as dst:
+            dst.write_band(1, data)
+        
+    """
+    IMPORT THE INDEPENDENT LAYERS FOR THE LAKES
+    """
+
+    def importVector(path, nameField):
+        shapes = dict()
+        with fiona.open(path, "r") as shapefile:
+            for feature in shapefile:
+                shapes[feature['properties'][nameField]] = dict()
+                shapes[feature['properties'][nameField]]["geometry"] = [feature["geometry"]]
+        return shapes
+
+    """
+    CREATE DATASET
+    """
+    def create_dataset(data, crs, transform):
+        # Receives a 3D array, a transform and a crs to create a rasterio dataset
+
+        memfile = MemoryFile()
+        dataset = memfile.open(driver='GTiff', height=data.shape[0], width=data.shape[1], count=1, crs=crs, 
+                               transform=transform, dtype=data.dtype)
+        dataset.write(data)
+
+        return dataset
+     
+    """
+    METHODS FOR DETECTING OUTLIERS THROUGH THRESHOLDS
+    """
+    def outlierRejectionIQR(self, out_path, minLower=None, maxUpper=None):
+        orDict = dict()       
+        
+        for key in self.crops:
+            orDict[key] = dict() 
+            # Compute the thresholds for the outlier rejection method
+            IQR = self.stats[key]['percentile_75'] - self.stats[key]['percentile_25']
+            # Verify that the computed thresholds are smaller/greater than the min/max arguments (if input)
+            lowerBound = self.stats[key]['percentile_25'] - 1.5*IQR
+            upperBound = self.stats[key]['percentile_75'] + 1.5*IQR
+            if ( minLower != None and lowerBound>minLower ):
+                lowerBound = minLower
+            if ( maxUpper != None and upperBound<maxUpper ):
+                upperBound = maxUpper
+
+            raster = self.crops[key]['crop']
+
+            # Identify the number of outliers for each threshold
+            countLower = sum(sum(sum(raster<=lowerBound)))
+            countUpper = sum(sum(sum(raster>=upperBound)))
+            countTotal = countLower + countUpper
+            # Extract the outliers values to the corresponding thresholds
+            raster[raster<=lowerBound] = np.nan
+            raster[raster>=upperBound] = np.nan
+            percValid = countTotal / raster.size
+
+            # Extract outliers from the crop raster
+            outliers = self.crops[key]['crop']
+            outliers[outliers<=lowerBound] = np.nan
+            outliers[outliers>=upperBound] = np.nan        
+
+            # Organize results in dictionary
+            orDict[key]['IQR'] = IQR
+            orDict[key]['lowerBound'] = lowerBound
+            orDict[key]['upperBound'] = upperBound
+            orDict[key]['countLower'] = countLower
+            orDict[key]['countUpper'] = countUpper
+            orDict[key]['countTotal'] = countTotal
+            orDict[key]['percValid'] = percValid
+            orDict[key]['percOutliers'] = 1 - percValid
+
+            # Save raster with no outliers
+            orDict[key]['raster'] = wqp.create_dataset(raster, self.image.crs, self.crops[key]['transform'])
+            orDict[key]['outliers'] = wqp.create_dataset(outliers, self.image.crs, self.crops[key]['transform'])
+        
+        return orDict
+        
+    
+    def outlierRejectionSigma(src, transform, out_path, stats, n_std):
+        
+        # Compute the thresholds for the outlier rejection method
+        lowerBound = stats.mean - n_std*stats.std
+        upperBound = stats.mean + n_std*stats.std
+        
+        # Identify the number of outliers for each threshold
+        countLower = sum(raster<=lowerBound)
+        countUpper = sum(raster<=lowerBound)
+        countTotal = countLower + countUpper
+        percOutliers = countTotal / cropped_image.size
+        
+        # Extract the outliers values to the corresponding thresholds
+        raster = src.image.read(1)
+        raster[raster<=lowerBound] = np.nan
+        raster[raster>=upperBound] = np.nan
+    
+        
+    """
+    CROP RASTER LAYER BY FEATURES
+    """
+    def cropRasterByFeatures(self, vectorData_path, nameField):
+        crops = dict()
+        shapes = wqp.importVector(vectorData_path, nameField)
+        for key in shapes:
+            crop_shape = dict()
+            featureName = key
+            featureGeometry = shapes[key]['geometry']
+            cropped_image, cropped_transform = rasterio.mask.mask(self.image, featureGeometry, crop=True)
+            crop_shape['crop'] = cropped_image
+            crop_shape['transform'] = cropped_transform
+            crops[key] = crop_shape
+        self.crops = crops
             
-   
